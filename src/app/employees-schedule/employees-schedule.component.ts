cb6e6a01a5db24cfcb2905ecdbc77c029a527616
Artur Tomczak <221666@edu.p.lodz.pl>
2019-03-12 13:31:57 +0100

    selecting multiple cells completed

--- src/app/employees-schedule/employees-schedule.component.ts
+++ src/app/employees-schedule/employees-schedule.component.ts
@@ -1,10 +1,10 @@
 import {Component, OnInit, Input} from '@angular/core';
 import * as moment from 'moment';
 
-import { CalendarDatesService } from '../services/calendar-dates.service';
-import { EmployeesService } from '../services/employees.service';
-import { Shifts } from '../shifts';
-import { Leaves } from '../leaves';
+import {CalendarDatesService} from '../services/calendar-dates.service';
+import {EmployeesService} from '../services/employees.service';
+import {Shifts} from '../shifts';
+import {Leaves} from '../leaves';
 
 @Component({
   selector: 'app-employees-schedule',
@@ -12,11 +12,18 @@ import { Leaves } from '../leaves';
   styleUrls: ['./employees-schedule.component.css']
 })
 export class EmployeesScheduleComponent implements OnInit {
+
+  constructor(private calendarDatesService: CalendarDatesService,
+              private employeesService: EmployeesService
+  ) { }
+
   private currentMonth = moment().format('MM');
   private currentYear = moment().format('YYYY');
   shiftStart = '06:00';
   shiftEnd = '18:00';
 
+  x: null;
+  y: null;
   calendarDaysArr: any;
   selectedField: string;
   contextMenu = false;
@@ -26,20 +33,26 @@ export class EmployeesScheduleComponent implements OnInit {
   dayClass: any;
   leaveOptionsContext = false;
 
-  x: null;
-  y: null;
-
-  a = false;
-
+  selectedCells = false;
   @Input() cmWidth: number;
 
-  constructor(private calendarDatesService: CalendarDatesService,
-              private employeesService: EmployeesService
-              ) {
-  }
+  minDayIdx: number;
+  minEmpIdx: number;
+  maxDayIdx: number;
+  maxEmpIdx: number;
+  buforEmpIdx: number;
+  buforDayIdx: number;
 
   shiftModel = new Shifts(1, '06:00', '18:00', null);
   leaveModel = new Leaves();
+
+  ngOnInit() {
+    this.calendarDaysArr = this.calendarDatesService.calendarDays(this.currentMonth, this.currentYear);
+    this.employeesService.getEmployees()
+      .subscribe(data => this.employees = data);
+  }
+
+
 
   onRightClick(event, dayIdx, empIdx) {
     const selectedDate = this.calendarDaysArr[dayIdx].i + '-' + this.currentMonth + '-' + this.currentYear;
@@ -53,17 +66,16 @@ export class EmployeesScheduleComponent implements OnInit {
     this.selectedField = dayIdx + '-' + empIdx;
   }
 
+  showShiftWindow() {
+    this.contextMenu = false;
+    this.showOptions = true;
+  }
+
   disableContextMenu() {
     this.contextMenu = false;
     this.showOptions = false;
     this.selectedField = null;
     this.leaveOptionsContext = false;
-  }
-
-  showShiftWindow() {
-    this.contextMenu = false;
-    this.showOptions = false;
-    this.showOptions = true;
   }
 
   addShift(even) {
@@ -77,25 +89,38 @@ export class EmployeesScheduleComponent implements OnInit {
     this.disableContextMenu();
   }
 
-  mouseDown() {
-    this.a = true;
+  minMaxIdx(minDayIdx, minEmpIdx, maxDayIdx, maxEmpIdx) {
+    this.minDayIdx = minDayIdx;
+    this.minEmpIdx = minEmpIdx;
+    this.maxDayIdx = maxDayIdx;
+    this.maxEmpIdx = maxEmpIdx;
   }
 
-  async mouseMove(msg) {
-    if (this.a === true && this.selectedField !== msg) {
-      console.log(msg);
-      this.selectedField = msg;
+  mouseDown(dayIdx, empIdx) {
+    this.buforDayIdx = dayIdx;
+    this.buforEmpIdx = empIdx;
+    this.minMaxIdx(dayIdx, empIdx, dayIdx, empIdx);
+    this.selectedCells = true;
+  }
+
+  mouseMove(dayIdx, empIdx) {
+    if (this.selectedCells === true) {
+      const msg = dayIdx + '-' + empIdx;
+      if (this.selectedField !== (msg)) {
+        if (dayIdx >= this.buforDayIdx && empIdx >= this.buforEmpIdx) {
+          this.minMaxIdx(this.buforDayIdx, this.buforEmpIdx, dayIdx, empIdx);
+
+        } else if (dayIdx < this.buforDayIdx && empIdx < this.buforEmpIdx) {
+          this.minMaxIdx(dayIdx, empIdx, this.buforDayIdx, this.buforEmpIdx);
+
+        } else if (dayIdx >= this.buforDayIdx && empIdx < this.buforEmpIdx) {
+          this.minMaxIdx(this.buforDayIdx, empIdx, dayIdx, this.buforEmpIdx);
+        } else {
+          this.minMaxIdx(dayIdx, this.buforEmpIdx, this.buforDayIdx, empIdx);
+        }
+        this.selectedField = msg;
+      }
     }
-  }
 
-  mouseUP() {
-    this.a = false;
-  }
-
-
-  ngOnInit() {
-    this.calendarDaysArr = this.calendarDatesService.calendarDays(this.currentMonth, this.currentYear);
-    this.employeesService.getEmployees()
-      .subscribe(data => this.employees = data);
   }
 }
